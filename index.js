18da3de93d7952d1eb91af0e52cf676fdaa4205f
T.R.Shashwath <shashwath@lucidindia.com>
2020-07-13 14:59:47 +0530

    Finished upload work.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 var app = new Vue({
     el: '#app',
     data: {
@@ -8,10 +9,13 @@ var app = new Vue({
 	address: null,
 	city: null,
 	profession: null,
+	dataSubmitted: false,
+	submitting: false,
     },
     methods: {
 	checkForm: function(e) {
 	    error = false;
+	    this.submitting = true;
 
 	    this.errors = [];
 
@@ -29,6 +33,30 @@ var app = new Vue({
 	    if (this.email && !this.validEmail(this.email)) {
 		this.errors.push('Please enter a valid e-mail ID');
 	    }
+
+	    const url = 'https://g0y0pcgx36.execute-api.us-west-2.amazonaws.com/default';
+
+	    const data = {
+		MobileNumber: this.mobile,
+		Name: this.name,
+		Address: this.address,
+		ProfessionalCategory: this.profession,
+		EMail: this.email,
+	    }
+
+	    var apigClient = apigClientFactory.newClient({
+		apiKey: 'M5vp7oAwFK9Nxa0PEwojT9qu4krGM4jQ4DLsrIFm'
+	    });
+
+	    apigClient.saveToDashboardPost([], data, {}).then(function(result) {
+		console.log(result);
+		this.dataSubmitted = true;
+		this.submitting = false;
+
+	    }.bind(this)).catch(function(result) {
+		console.log(result);
+		this.submitting = false;
+	    });
 
 	    e.preventDefault();
 	},
